edcaa87d3b30c7e27775cec1f93fac75d4c63a4b
juseedom <juseedom@gmail.com>
2014-02-09 22:26:35 +0800

    Update RFDB.py
    Finished the beta version for construct RF Database

--- RFMasterList/RFDB.py
+++ RFMasterList/RFDB.py
@@ -4,7 +4,7 @@ from difflib import get_close_matches
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-keywords = ('index','eNodeB ID_Sector No.','eNodeB Id','Lat','Long', 'Cell Id', 'Sector Id','EARFCN','TAL','TAI')
+keywords = ('Index','eNodeB Id','Lat','Long', 'Cell Id', 'Sector Id','EARFCN','TAL','TAI')
 
 
 class RFDataBase():
@@ -13,35 +13,37 @@ class RFDataBase():
     Able to read RF data from Excel files;
     
     """
-    def __init__(self, filepath):
+    def __init__(self):
+        self.DB = dict()
+
+            
+    def readFile(self, filepath):
         """
         For xlsx file, format is not read due to xlrd limit
         The default sheet index set to 0
         """
         try:
             if filepath.split(".")[-1] == "xls":
-                rf_wb = open_workbook(filepath, formatting_info = True, on_demand = True)
-                
+                self.rf_wb = open_workbook(filepath, formatting_info = True, on_demand = True)
             elif filepath.split(".")[-1] == "xlsx":
-                rf_wb = open_workbook(filepath, formatting_info = False, on_demand = True)
+                self.rf_wb = open_workbook(filepath, formatting_info = False, on_demand = True)
             else:
-                print "Unknown File Type."
-                return False
-            self.DB = dict()
-            self.readFile(rf_wb)
-                
+                logging.debug("Unknown File Type.")
+                self.rf_wb = False              
         except TypeError:
-            print "Cannot Find the Excel File."
+            logging.debug("Cannot Find the Excel File.")
         except IOError:
-            print "Open Excel File Fialed."
-        
-        
-            
-            
-    def readFile(self, rf_wb, sheet_index = 0):
+            logging.debug("Open Excel File Fialed.")
+        finally:
+            if self.rf_wb:
+                return self.rf_wb.sheet_names()
+            else:
+                return False
+
+    def readSheet(self, sheet_index = 0):
         """Auto match the title rows according to keyword
         transfer RF data from workbook into dict"""
-        rf_sheet = rf_wb.sheet_by_index(sheet_index)
+        rf_sheet = self.rf_wb.sheet_by_index(sheet_index)
         
         #find the title according to keywords from first 5 rows
         start_row = 0
@@ -56,16 +58,17 @@ class RFDataBase():
             if s > max_match:
                 start_row = i
                 max_match = s
+
         logging.debug("Found Title on row %i according to keyword match." %(start_row+1))
         
         #read title
         #match index or cell name and check duplicate
-        str_title = rf_sheet.row_values(start_row)
+        self.str_title = rf_sheet.row_values(start_row)
         title_index = -1
-        for i in ("index", "CellName"):
-            a = get_close_matches(i, str_title)            
+        for i in ("Index", "CellName"):
+            a = get_close_matches(i, self.str_title)            
             if a:
-                title_index = str_title.index(a[0])
+                title_index = self.str_title.index(a[0])
                 str_index = rf_sheet.col_values(title_index)[start_row+1:]                
                 if [b for b in str_index if str_index.count(b)>1]:
                     title_index = -1
@@ -80,7 +83,7 @@ class RFDataBase():
             raw_data = dict()
             for i in range(start_row+1, rf_sheet.nrows):
                 raw_value = rf_sheet.row_values(i)
-                raw_data = dict(zip(str_title, raw_value))
+                raw_data = dict(zip(self.str_title, raw_value))
                 str_index = rf_sheet.cell(i, title_index).value
                 self.DB[str_index] = raw_data
             logging.debug(len(self.DB))
@@ -88,8 +91,42 @@ class RFDataBase():
             logging.debug("Cannot find the index")
         
          #format type (not suggested)
+
+        return self.str_title
+
+
+    def loadMappingTitle(self, title):
+        self.mapTitle = dict()
         
-    def writeFile(self):
+
+
+    def DBStatistics(self, dedicate_index):
+        """
+        Remove all those duplicated value for specific Parameters
+        1st Step: remove duplicated objects
+        2nd Step: remove duplicated values
+
+        some will transfer value type while all others remain str:
+        EARFCN->str->float
+
+        """
+
+        logging.debug("Start to statistics for %s" %dedicate_index)
+        dedicate_value = set([str(raw_data[dedicate_index]) for raw_data in self.DB.values()])
+        
+
+        if dedicate_index == "EARFCN":
+            logging.debug("Start to stransfer All EARFCN value into Float")
+            #dedicate_value = set([float(i) for i in dedicate_value])
+            dedicate_value = set([i.split('.')[0] for i in dedicate_value])
+
+        return dedicate_value
+
+
+
+
+
+    def Update2File(self):
         pass
             
         
@@ -97,7 +134,8 @@ class RFDataBase():
 
 
 if __name__ == "__main__":
-    test = RFDataBase("/home/juseedom/Downloads/RF_MasterList20140207.xls")
+    test = RFDataBase("D:\\CODES\\Python\\RFMasterList\\RFMasterList\\RFMasterList\\Test File\\RF_MasterList20140207.xls")
+    print test.DBStatistics("SiteAddress")
     
 
     
