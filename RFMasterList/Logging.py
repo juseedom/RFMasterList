f82600be1e6f1eb0cf8a331db5130fdc671e8047
juseedom <juseedom@gmail.com>
2014-02-28 00:02:07 +0800

    Add read and write rules to cfg file

--- RFMasterList/Logging.py
+++ RFMasterList/Logging.py
@@ -1,5 +1,6 @@
 import ConfigParser
 import os
+import json
 
 class RFLogging():
     def __init__(self):
@@ -11,20 +12,69 @@ class RFLogging():
         #currentpath = os.path.dirname(os.path.abspath(__file__))
         if os.path.isfile("Excel2KML.cfg"):
             #read configuration
-            self.config = ConfigParser.RawConfigParser()
-            self.config.read("Excel2KML.cfg")
-            self.config.get("section", "option")
             
-            
+            with open("Excel2KML.cfg", "rb") as configfile:
+                self.readRuleFromCfg(configfile)
+
         else:
-            self.config = ConfigParser.RawConfigParser()
-            self.config.add_section("section")
-            self.config.set("section", "option", "value")
+            self.createDefaultRule()
+            self.writeRuleToCfg()
+         
+         
+    def createDefaultRule(self):
+        #build default rules
+        rf_item = dict()
+        #read RFDB
+        
+        #change to tab_setting, do KPI statistics now
+        rf_item["EARFCN"] = ["1850", "2970"]#self.RFDB.DBStatistics("EARFCN")
+        rf_item["Type"] = ["Indoor", "Outdoor"]#self.RFDB.DBStatistics("Type")
+        rf_item["PCI"] = ['0', '1', '2']
+
+        kml_item = dict()
+        kml_item["Shape"] = ["Circle", "Sector"]
+        kml_item["Cell Radius"] = ["0.00025", "0.001265"]
+        kml_item["Height"] = list()
+        #kml_item["Shape"] = [["Circle","0.00025","20"],["Sector","0.001265","10"]]
+        kml_item["Line Color"] = list()
+        kml_item["Fill Color"] = list()
+
+        self.rules = dict()
+            
+        self.rules[("EARFCN", "Fill Color")] = dict().fromkeys(rf_item["EARFCN"], "blue")
+        self.rules[("PCI", "Line Color")] = dict().fromkeys(rf_item["PCI"], "red")
+        self.rules[("Type", "Shape")] = dict(zip(rf_item["Type"],kml_item["Shape"]))
+        self.rules[("Type", "Cell Radius")] = dict(zip(rf_item["Type"],kml_item["Cell Radius"]))
+        self.rules[("EARFCN", "Height")] = dict().fromkeys(rf_item["EARFCN"], "0")
+    
+    def writeRuleToCfg(self):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.add_section("RuleType")
+        self.config.add_section("RuleValue")
+        for i, type_list in enumerate(self.rules):
+            self.config.set("RuleType", "type%i" %i, json.dumps(type_list))
+            self.config.set("RuleValue", "value%i" %i,json.dumps(self.rules.values()[i]))
+            
                 
-                
-    def remember_cfg(self):
         with open("Excel2KML.cfg", "wb") as configfile:
             self.config.write(configfile)
+        print "Saving the CFG successfully"
+                
+    def readRuleFromCfg(self, configfile):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.readfp(configfile)        
+        self.rules = dict()
+        for i in range(len(self.config.options("RuleType"))):
+            rule_type = tuple(json.loads(self.config.get("RuleType", "type%i" %i)))
+            self.rules[rule_type] = json.loads(self.config.get("RuleValue", "value%i" %i))
+
+            
+if __name__ == "__main__":
+    test = RFLogging()
+    
+    
+    
+    
     
     
     
